eac791857b1010dd5a335a1c95f02c9dc9179940
Lily0318 <168014549+Lily0318@users.noreply.github.com>
2025-02-12 15:14:02 +0000

    updating the app v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,18 @@ if app_mode == "01 Introduction":
 
 elif app_mode == "02 Data Visualization":
     st.write("Let's start doing data viz") 
+
+## option 1 
+    list_of_variables = df.columns
+    user_selection = st.selectbox("Select a variable", list_of_variables)
+    st.bar_chart(df[user_selection])
+
+    user_selections = st.multiselect("Select a variable", list_of_variables)
+
+    fig, ax = 
+    sns.barplot
+
+## option 2
     list_of_var = df.columns
     user_selection = st.selectbox("Select a variable", list_of_var)
 
